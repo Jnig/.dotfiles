c93662b35d4d973087b1f9d49827e7e03efe09f4
Jakob Niggel <jakobniggel@gmail.com>
2022-11-17 14:18:10 +0100

    chore: format nvim config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,29 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-  return
+	return
 end
 
-telescope.setup()
-require('telescope').load_extension('fzf') -- load fzf native for improved performance
-require("telescope").load_extension('ui-select')
+local actions = require("telescope.actions")
+telescope.setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<C-w>"] = actions.send_selected_to_qflist,
+				["<C-q>"] = actions.send_to_qflist,
+			},
+			n = {
+				["<C-w>"] = actions.send_selected_to_qflist,
+				["<C-q>"] = actions.send_to_qflist,
+			},
+		},
+	},
+})
+require("telescope").load_extension("fzf") -- load fzf native for improved performance
+require("telescope").load_extension("ui-select")
 
+local builtin = require("telescope.builtin")
 
-
-
-local builtin = require('telescope.builtin')
-
-
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
