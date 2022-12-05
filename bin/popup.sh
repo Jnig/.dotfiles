2edc36fdc17eeea22884c0858050b139095bcdaa
Jakob Niggel <jakobniggel@gmail.com>
2022-12-05 10:57:27 +0100

    refactor: manage windows

--- bin/popup.sh
+++ bin/popup.sh
@@ -4,12 +4,13 @@ TITLE=mylauncher
 
 SCREEN_WIDTH=`yabai -m query --displays --display | jq .frame.w*2`
 SCREEN_HEIGHT=`yabai -m query --displays --display | jq .frame.h*2`
+SCREEN_OFFSET=`yabai -m query --displays --display | jq .frame.x*2`
 
 TERM_WIDTH=1280
-TERM_HEIGHT=1260
+TERM_HEIGHT=800
 
-let "X=SCREEN_WIDTH/2-TERM_WIDTH/2"
-let "Y=SCREEN_HEIGHT/2-TERM_HEIGHT/2"
+let "X=SCREEN_WIDTH/2+SCREEN_OFFSET/4-TERM_WIDTH/2"
+let "Y=(SCREEN_HEIGHT/2-TERM_HEIGHT/2)-TERM_HEIGHT"
 
 alacritty -v -t "${TITLE}" -o window.position.x="${X}" -o window.position.y="${Y}" -o window.dimensions.lines=35 -o window.dimensions.columns=80 -o window.decorations=none --working-directory "$(pwd)" -e "$1"
 
