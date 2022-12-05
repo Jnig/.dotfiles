3af114351ef192b37886570bd36739db7f99739f
Jakob Niggel <jakobniggel@gmail.com>
2022-12-05 10:50:56 +0100

    refactor: spaces

--- config/yabai/spaces.sh
+++ config/yabai/spaces.sh
@@ -6,14 +6,6 @@ if [[ $(yabai -m query --displays | jq '.[]|.id' | wc -l)  -ge 2 ]]; then
   MULTI=1	
 fi
 
-# cleanup
-UNUSED=($(yabai -m query --spaces | jq -r '.[]|select(.label == "")|.index' | tr '\n' ' '))
-for i in "${UNUSED[@]}"
-do
- echo "removing $i"
- yabai -m space "$i" --destroy
-done
-
 
 function setup_space {
   local idx="$1"
@@ -26,28 +18,28 @@ function setup_space {
     yabai -m space --create
   fi
 
-  yabai -m space "$idx" --label "$name"
-  yabai -m space "$name" --move "$idx"
-  yabai -m space "$name" --display "$display"
+  yabai -m space "$idx" --move "$idx"
+  yabai -m space "$idx" --display "$display"
 }
 
 
 if [[ "$MULTI" -eq "1" ]]; then 
-  setup_space 1 space.7 1
-  setup_space 2 space.1 2
-  setup_space 3 space.2 2
-  setup_space 4 space.3 2
-  setup_space 5 space.4 2
-  setup_space 6 space.5 2
-  setup_space 7 space.6 2
+  setup_space 1 1
+  setup_space 2 2
+  setup_space 3 2
+  setup_space 4 2
+  setup_space 5 2
+  setup_space 6 2
+  setup_space 7 2
+  setup_space 7 2
 else 
-  setup_space 1 space.1 1
-  setup_space 2 space.2 1
-  setup_space 3 space.3 1
-  setup_space 4 space.4 1
-  setup_space 5 space.5 1
-  setup_space 6 space.6 1
-  setup_space 7 space.7 1
+  setup_space 1 1
+  setup_space 2 1
+  setup_space 3 1
+  setup_space 4 1
+  setup_space 5 1
+  setup_space 6 1
+  setup_space 7 1
 fi
 
 brew services restart sketchybar
