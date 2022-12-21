81b617bb6893df66bd93cfcffbf51c623983311d
Jakob Niggel <jakobniggel@gmail.com>
2022-12-21 15:27:44 +0100

    feat: use volar take over mode insteaf of typescript server

--- config/nvim/lua/lsp/mason.lua
+++ config/nvim/lua/lsp/mason.lua
@@ -1,38 +1,37 @@
 local servers = {
-  "sumneko_lua",
-  "tsserver",
-  "volar",
-  -- "bashls",
-  "jsonls",
-  -- "yamlls",
+	"sumneko_lua",
+	--  "tsserver",
+	"volar",
+	-- "bashls",
+	"jsonls",
+	-- "yamlls",
 }
-
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = servers,
-  automatic_installation = true,
+	ensure_installed = servers,
+	automatic_installation = true,
 })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
-  return
+	return
 end
 
 local opts = {}
 
 for _, server in pairs(servers) do
-  opts = {
-    on_attach = require("lsp.handlers").on_attach,
-    capabilities = require("lsp.handlers").capabilities,
-  }
+	opts = {
+		on_attach = require("lsp.handlers").on_attach,
+		capabilities = require("lsp.handlers").capabilities,
+	}
 
-  server = vim.split(server, "@")[1]
+	server = vim.split(server, "@")[1]
 
-  local require_ok, conf_opts = pcall(require, "lsp.settings." .. server)
-  if require_ok then
-    opts = vim.tbl_deep_extend("force", conf_opts, opts)
-  end
+	local require_ok, conf_opts = pcall(require, "lsp.settings." .. server)
+	if require_ok then
+		opts = vim.tbl_deep_extend("force", conf_opts, opts)
+	end
 
-  lspconfig[server].setup(opts)
+	lspconfig[server].setup(opts)
 end
