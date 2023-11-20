fd85e2eea715e67ac9a3f32eb17990bdc8468875
Jakob Niggel <jakob.niggel@vitesco.com>
2023-11-20 07:14:09 +0100

    refactor: use lazyvim

--- config/nvim/lua/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
-vim.opt.termguicolors = true
-
-require("bufferline").setup({
-  highlights = require("catppuccin.groups.integrations.bufferline").get(),
-  options = {
-    show_buffer_icons = true,
-    offsets = { { filetype = "neo-tree", text = "neo-tree", padding = 1 } },
-  },
-})
