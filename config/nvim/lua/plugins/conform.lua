868090d4a7593c05af6c6639556169876c177584
Jakob Niggel <jakobniggel@gmail.com>
2024-03-01 14:53:19 +0100

    feat: prettierd for additional file types

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -3,6 +3,12 @@ return {
   opts = {
     formatters_by_ft = {
       javascript = { "prettierd" },
+      typescript = { "prettierd" },
+      vue = { "prettierd" },
+      yaml = { "prettierd" },
+      json = { "prettierd" },
+      markdown = { "prettierd" },
+      html = { "prettierd" },
     },
     log_level = vim.log.levels.DEBUG,
   },
